7e4c90ba69931a1cddb0e563bb403f60c98a9ad5
Teodora Palacian <palacian.teodora@gmail.com>
2020-12-16 16:50:40 +0200

    lab11

--- Palacian_Ioana_Teodora_lab10/ListPage.xaml.cs
+++ Palacian_Ioana_Teodora_lab10/ListPage.xaml.cs
@@ -26,7 +26,22 @@ namespace Palacian_Ioana_Teodora_lab10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
-            public ListPage()
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+        public ListPage()
         {
             InitializeComponent();
         }
